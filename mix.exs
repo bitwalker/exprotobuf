219a13b41284c1d3ccacf4dc22668067162c39c8
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-05-07 13:15:15 -0500

    Version 0.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Protobuf.Mixfile do
 
   def project do
     [app: :exprotobuf,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 0.13.2-dev",
      description: description,
      package: package,
