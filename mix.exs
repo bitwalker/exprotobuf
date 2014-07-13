c060b1a793587f9c57389442b95f1e4c7d1c0018
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-07-12 23:46:53 -0500

    Version 0.7.0

--- mix.exs
+++ mix.exs
@@ -3,16 +3,13 @@ defmodule Protobuf.Mixfile do
 
   def project do
     [app: :exprotobuf,
-     version: "0.6.1",
-     elixir: "~> 0.14.2",
+     version: "0.7.0",
+     elixir: "~> 0.14.3",
      description: description,
      package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: []]
   end
@@ -31,15 +28,6 @@ defmodule Protobuf.Mixfile do
       links: [ {"GitHub", "https://github.com/bitwalker/exprotobuf"} ] ]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  # {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  # {:foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{ :gpb, github: "tomas-abrahamsson/gpb" }]
   end
