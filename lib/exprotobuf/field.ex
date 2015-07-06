14e18a292cfa52487e4fadeb3cb90650e44b1d72
Janne Kaistinen <janneka@JanneAir.local>
2015-07-06 23:22:09 +0300

    Fix compile error trying to extract ?gpb_field record from gpb.hrl

--- lib/exprotobuf/field.ex
+++ lib/exprotobuf/field.ex
@@ -1,5 +1,5 @@
 defmodule Protobuf.Field do
-  @record Record.Extractor.extract(:"?gpb_field", from: Path.join([Mix.Project.deps_path, "gpb", "include", "gpb.hrl"]))
+  @record Record.Extractor.extract(:field, from: Path.join([Mix.Project.deps_path, "gpb", "include", "gpb.hrl"]))
   defstruct @record
 
   def record, do: @record
