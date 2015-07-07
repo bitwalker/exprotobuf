bcd8144c5fcea3aaab8cce2a9035e781d38e02ae
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-07-06 19:05:21 -0500

    Handle backwards incompatible change in Record.extract

--- lib/exprotobuf/field.ex
+++ lib/exprotobuf/field.ex
@@ -1,5 +1,10 @@
 defmodule Protobuf.Field do
-  @record Record.Extractor.extract(:"?gpb_field", from: Path.join([Mix.Project.deps_path, "gpb", "include", "gpb.hrl"]))
+  case Version.compare(System.version, "1.0.4") do
+    :gt ->
+      @record Record.Extractor.extract(:field, from: Path.join([Mix.Project.deps_path, "gpb", "include", "gpb.hrl"]))
+    _ ->
+      @record Record.Extractor.extract(:"?gpb_field", from: Path.join([Mix.Project.deps_path, "gpb", "include", "gpb.hrl"]))
+  end
   defstruct @record
 
   def record, do: @record
