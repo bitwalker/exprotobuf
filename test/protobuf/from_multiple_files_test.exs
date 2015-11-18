889fe924a9faa529ad0c2046d98a119bc015c92f
Michael Ries <michael@riesd.com>
2015-11-17 23:22:02 -0700

    test to cover inject true and false

--- test/protobuf/from_multiple_files_test.exs
+++ test/protobuf/from_multiple_files_test.exs
@@ -1,12 +1,25 @@
 defmodule Protobuf.FromMultipleFiles.Test do
   use Protobuf.Case
 
-  defmodule Broker do
-    use Protobuf, from: Path.wildcard(Path.expand("../proto/broker/*.proto", __DIR__))
+  defmodule TopLevel do
+    use Protobuf, from: [Path.expand("../proto/basic.proto", __DIR__),
+                         Path.expand("../proto/mumble.proto", __DIR__)]
   end
 
-  test "all the messages are generated" do
-    assert %Broker.Error{field: "test"} = Broker.Error.new(field: "test")
-    assert %Broker.Exchange{id: "test"} = Broker.Exchange.new(id: "test")
+  test "generates all messages nested under TopLevel" do
+    assert %{f1: 255} = TopLevel.Basic.new(f1: 255)
+    assert %{version: 127} = TopLevel.MumbleProto.Version.new(version: 127)
+    assert %{packet: "abc"} = TopLevel.MumbleProto.UDPTunnel.new(bytes: "abc")
+  end
+
+  defmodule NoTopLevel do
+    use Protobuf, from: [Path.expand("../proto/basic.proto", __DIR__),
+                         Path.expand("../proto/mumble.proto", __DIR__)],
+                  inject: true
+  end
+
+  test "generates all messages without nesting under NoTopLevel" do
+    assert %{reason: "deal with it"} = Authorization.WrongAuthorizationHttpMessage.new(reason: "deal with it")
+    assert %{authorization: "I do what I want"} = Chat.WebsocketServerContainer.new(authorization: "I do what I want")
   end
 end
