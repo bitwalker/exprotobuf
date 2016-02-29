2d1b384ada648d80567eb3d8453fbafd39dbd66a
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-02-28 19:48:21 -0600

    Add fallback impl of Protobuf.Serializable. Fixes #41

--- lib/serializable.ex
+++ lib/serializable.ex
@@ -2,9 +2,15 @@ defprotocol Protobuf.Serializable do
   @moduledoc """
   Defines the contract for serializing protobuf messages.
   """
+  @fallback_to_any true
 
   @doc """
   Serializes the provided object as a protobuf message in binary form.
   """
   def serialize(object)
+end
+
+defimpl Protobuf.Serializable, for: Any do
+  def serialize(%{__struct__: module} = obj), do: module.encode(obj)
+  def serialize(_), do: {:error, :not_serializable}
 end
