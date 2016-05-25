cb848ce2fc9376df6f1676960f40cd4d63468d14
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-05-25 11:02:14 +0530

    Added tests for decode

--- test/protobuf/delimited_test.exs
+++ test/protobuf/delimited_test.exs
@@ -3,14 +3,15 @@ defmodule Extprotobuf.Delimited.Test do
 
   defmodule Wrapper do
     use Protobuf, """
-      message User {
-        required string name = 1;
-        optional int32 id = 2;
-      }
+    message User {
+    required string name = 1;
+    optional int32 id = 2;
+    }
     """
   end
 
   @encoded_out <<0, 0, 0, 9, 10, 5, 77, 117, 106, 106, 117, 16, 1>>
+  @encoded_out_multiple <<0, 0, 0, 9, 10, 5, 77, 117, 106, 106, 117, 16, 1, 0, 0, 0, 9, 10, 5, 77, 117, 106, 106, 117, 16, 1, 0, 0, 0, 9, 10, 5, 77, 117, 106, 106, 117, 16, 1>>
 
   test "encode creates a valid message for 1 message" do
     user = %Wrapper.User{name: "Mujju", id: 1}
@@ -33,6 +34,20 @@ defmodule Extprotobuf.Delimited.Test do
     assert encoded_bytes == String.duplicate(size <> encoded_user, 3)
   end
 
+  test "decode creates a valid struct for 1 message" do
+    assert Protobuf.Delimited.decode(@encoded_out, Wrapper.User) == [%Wrapper.User{name: "Mujju", id: 1} | nil ]
+  end
+
+  test "decode creates a valid struct for 3 message" do
+    users = Protobuf.Delimited.decode(@encoded_out_multiple, Wrapper.User)
+    [h|rest] = users
+    assert h == %Wrapper.User{name: "Mujju", id: 1}
+    [h|rest] = rest
+    assert h == %Wrapper.User{name: "Mujju", id: 1}
+    [h|rest] = rest
+    assert h == %Wrapper.User{name: "Mujju", id: 1}
+    assert rest == nil
+  end
 
 end
 
