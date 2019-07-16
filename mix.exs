36dc21c075140ba35586d89feca23bbb64ae8e9c
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2019-07-15 22:21:32 -0400

    Version 1.2.17

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Protobuf.Mixfile do
 
   def project do
     [app: :exprotobuf,
-     version: "1.2.16",
+     version: "1.2.17",
      elixir: "~> 1.7",
      elixirc_paths: elixirc_paths(Mix.env),
      preferred_cli_env: [
